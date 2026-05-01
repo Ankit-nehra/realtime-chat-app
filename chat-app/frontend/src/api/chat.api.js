2bf4e90ec0f63ca0260aab1cfc5b99b4ab79ed86
Ankit-nehra <ankitnehra7000@gmail.com>
2026-05-01 19:36:06 +0530

    Update chat.api.js

--- chat-app/frontend/src/api/chat.api.js
+++ chat-app/frontend/src/api/chat.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/chat",
+  baseURL: "https://api-gateway-service-thia.onrender.com/api/chat",
 });
 
 export const sendMessage = (data) => API.post("/send", data);
