fef6da7c8648acc34ae90b17d6fb305d919cf54e
Ankit-nehra <ankitnehra7000@gmail.com>
2026-05-01 19:36:39 +0530

    Update user.api.js

--- chat-app/frontend/src/api/user.api.js
+++ chat-app/frontend/src/api/user.api.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/users",
+  baseURL: "https://api-gateway-service-thia.onrender.com/api/users",
 });
 
 export const getOnlineUsers = () => API.get("/online");
