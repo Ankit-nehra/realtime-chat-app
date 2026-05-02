1572ac20b45f622516160b91676d350857c47456
Ankit-nehra <ankitnehra7000@gmail.com>
2026-05-02 16:41:34 +0530

    Update auth.api.js

--- chat-app/frontend/src/api/auth.api.js
+++ chat-app/frontend/src/api/auth.api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://api-gateway-service-thia.onrender.com/api/auth",
+  baseURL: `${import.meta.env.VITE_API_URL}/api/auth`,
 });
 export const register = (data) => API.post("/register", data);
 export const login = (data) => API.post("/login", data);
