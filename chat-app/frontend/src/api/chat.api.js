2bc9c8c50d3b4aee96f73c081db895a11d48abef
Ankit-nehra <ankitnehra7000@gmail.com>
2026-05-02 16:44:12 +0530

    Update chat.api.js

--- chat-app/frontend/src/api/chat.api.js
+++ chat-app/frontend/src/api/chat.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api/chat',
+  baseURL: `${import.meta.env.VITE_API_URL}/api/chat`,
 });
 
 export const sendMessage = (data) => API.post("/send", data);
