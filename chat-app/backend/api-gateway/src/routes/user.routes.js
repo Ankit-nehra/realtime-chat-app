33d14506accab0a51b1220e4a34f5c7022f287a1
Ankit-nehra <ankitnehra7000@gmail.com>
2026-05-04 15:47:14 +0530

    Update user.routes.js

--- chat-app/backend/api-gateway/src/routes/user.routes.js
+++ chat-app/backend/api-gateway/src/routes/user.routes.js
@@ -5,9 +5,11 @@ const router = express.Router();
 
 const USER_SERVICE_URL = "https://user-service-tz4x.onrender.com/api/users";
 
-router.get("/online", async (req, res) => {
+
+//  GET ALL USERS
+router.get("/", async (req, res) => {
   try {
-    const response = await axios.get(`${USER_SERVICE_URL}/online`);
+    const response = await axios.get(`${USER_SERVICE_URL}/`);
     res.json(response.data);
   } catch (err) {
     res.status(500).json(err.message);
