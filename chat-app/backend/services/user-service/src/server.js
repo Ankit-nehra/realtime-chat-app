a2594e2d4becff9cfcaa13e6750ca09a1ca89700
Ankit-nehra <ankitnehra7000@gmail.com>
2026-05-04 15:55:03 +0530

    Update server.js

--- chat-app/backend/services/user-service/src/server.js
+++ chat-app/backend/services/user-service/src/server.js
@@ -1,11 +1,18 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import userRoutes from "./routes/user.routes.js";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// DB connect
+connectDB();
 
 // 🌍 GLOBAL LOGGER
 app.use((req, res, next) => {
